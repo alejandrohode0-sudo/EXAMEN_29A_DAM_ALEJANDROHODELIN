8c502554a6286c2d0e98e4c5d6916a405cc348f6
alejandrohode0-sudo <alejandrohode0@gmail.com>
2026-04-29 13:19:30 +0200

    Rama1 y codigo echo.

--- src/PrimerPaquete/ClaseMain.java
+++ src/PrimerPaquete/ClaseMain.java
@@ -6,7 +6,19 @@ public class ClaseMain {
 	public static void main(String[] args) {
 		System.out.println("Buenos dias Ceinmark, soy Mart Gomez");
 		int numero = new Scanner(System.in).nextInt();
-		if (numero % 2 == 0) System.out.println("Es un numero primo");
-		else System.out.println("No es un numero primo");
+		if (numero % 2 == 0) {
+			System.out.println("Es un numero primo");
+			System.out.println("Los divisores del numero son:");
+			while (numero % 2 == 0) {
+				System.out.println(numero);
+				numero /= 2;
+			}
+			System.out.println(numero);
+			System.out.println(numero / numero);
+		}
+		else {
+			System.out.println("No es un numero primo");
+		}
+		
 	}
 }
